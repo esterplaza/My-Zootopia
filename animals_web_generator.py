c772393be967862956e38e182de62e02bfd88b12
esterplaza <esterplaza@gmail.com>
2026-05-24 11:08:29 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -7,6 +7,18 @@ def load_data(file_path):
         return json.load(handle)
 
 
+def read_html_template(html_path):
+    """reads a html template file"""
+    with open(html_path, "r", encoding="utf-8") as html_file:
+        return html_file.read()
+
+
+def write_new_html(html_string):
+    """writes a new html file"""
+    with open("animals.html", "w", encoding="utf-8") as new_html_file:
+        new_html_file.write(html_string)
+
+
 def get_data_from_file(animal):
     """gets the value of the fields name, diet, first location and type from
     one animal of the animal data"""
@@ -20,14 +32,18 @@ def get_data_from_file(animal):
 
 def main():
     animals_data = load_data("animals_data.json")
+    output = ""
     for animal in animals_data:
         a_name, a_diet, a_location, a_type = get_data_from_file(animal)
         data = {"Name": a_name, "Diet": a_diet, "Location": a_location,
                 "Type": a_type}
         for label, value in data.items():
             if value:
-                print(f"{label}: {value}")
-        print()
+                output += f"{label}: {value}\n"
+        output += "\n"
+    html_data = read_html_template("animals_template.html")
+    html_new_data = html_data.replace("__REPLACE_ANIMALS_INFO__", output)
+    write_new_html(html_new_data)
 
 
 if __name__ == "__main__":
